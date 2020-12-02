45d7ac3727c2735897c8a337186765d4b551f534
unknown <femiabimbola6@gmail.com>
2020-12-02 16:51:57 +0100

    added process on

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ const PORT = process.env.PORT || 7000;
 // check what create server is
 const server = http.createServer(app);
 
+process.on("uncaughtException", (error, origin) => {
+    DEBUG(`The uncaught exception error is: ${error.message}`);
+    DEBUG(`The origin of the error is at : ${origin}`)
+    process.exit(1)
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+    DEBUG(`The reason the promise is rejected is: ${reason.message}`);
+    DEBUG( `The promise rejected is:  ${promise}`);
+    process.exit(1);
+    });
+
 
 server.listen(PORT, () => {
     DEBUG(`Server is running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`)
 })
-
-
-
-
-
-
-
