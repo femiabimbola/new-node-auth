d2bf44d0d0d8dfc885f9c75a4384b6794eb06136
unknown <femiabimbola6@gmail.com>
2020-12-10 16:41:39 +0100

    userschema edited

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 config();
 
-const jwtPrivateSecret = process.env.JWT_PRIVATE_SECRET
+const jwtPrivateSecret = process.env.JWT_PRIVATE_SECRET;
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -24,16 +24,28 @@ const userSchema = new mongoose.Schema({
       },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.password || !this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, parseInt(process.env.HASH) );
+  next();
+});
 
 userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
-  };
+};
   
-  userSchema.methods.generateVerificationToken = function () {
+userSchema.methods.generateVerificationToken = function () {
     return jwt.sign({ id: this._id }, jwtPrivateSecret, {
       expiresIn: "10d",
       algorithm: "RS256",
     });
-  };
+};
+
+userSchema.statics.checkExistingField = async (field, value) => {
+  const checkField = await User.findOne({ [`${field}`]: value });
+
+  return checkField;
+};
 
 export default mongoose.model('User', userSchema)
